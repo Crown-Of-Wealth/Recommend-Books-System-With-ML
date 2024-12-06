b8097f86f359000386946509f22782e2c9323af8
Oluwabunmi Adeola Ogunlana <ogunlanaoluwabunmi@gmail.com>
2024-12-06 15:41:11 +0100

    setup file updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+REPO_NAME = 'Recommend-Books-System-With-ML'
+AUTHOR_USER_NAME = 'Crown-Of-Wealth'
+SRC_REPO = 'src'
+LIST_OF_REQUIREMENTS = ['streamlit', 'numpy']
+
