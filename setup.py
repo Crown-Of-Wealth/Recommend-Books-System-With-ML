d79b23fc0fe28ef39bca5d364d2961875f08d5aa
Oluwabunmi Adeola Ogunlana <ogunlanaoluwabunmi@gmail.com>
2024-12-06 15:51:23 +0100

    setup file updated 2

--- setup.py
+++ setup.py
@@ -8,3 +8,15 @@ AUTHOR_USER_NAME = 'Crown-Of-Wealth'
 SRC_REPO = 'src'
 LIST_OF_REQUIREMENTS = ['streamlit', 'numpy']
 
+setup(name=SRC_REPO,
+      version='0.0.1',
+      author=AUTHOR_USER_NAME,
+      description='A small package for Recommending Books',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url=f'https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}',
+      author_email='ogunlanaoluwabunmi@gmail.com',
+      packages=[SRC_REPO],
+      python_requires='>=3.10',
+      install_requires=LIST_OF_REQUIREMENTS
+)
