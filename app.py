c4eefdd4918862c7d92435956d958fdbf2c20267
Oluwabunmi Adeola Ogunlana <ogunlanaoluwabunmi@gmail.com>
2024-12-08 23:40:43 +0100

    load books_name

--- app.py
+++ app.py
@@ -4,3 +4,4 @@ import numpy as np
 
 st.header('Books Recommendation System using Machine Learning')
 model = pickle.load(open('artifacts/model.pkl', 'rb'))
+books_name = pickle.load(open('artifacts/books_name.pkl', 'rb'))
